dbd622f7f480746205306e6b69d0e91fdfb1915f
Damaris Göbel <damaris.goebel@gmx.de>
2020-09-10 20:28:08 +0200

    broken our homepage to smaller components, styling our menu items

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
 
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
     <div>
-    {/* Hello Stranger Damaris Why can't I push*/}
 <HomePage/>
     </div>
   );
