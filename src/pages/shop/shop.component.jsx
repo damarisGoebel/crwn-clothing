670101255542421c0413617289bf6548b345111f
Damaris Göbel <damaris.goebel@gmx.de>
2020-10-10 17:38:07 +0200

    added collections-overview component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,32 +1,14 @@
 import React from 'react';
 
-import SHOP_DATA from './shop.data';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import CollectionOverview from '../../components/collections-overview/collections-overview.component';
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
+const ShopPage = ({collections}) => (
+    <div className='shop-page'>
+        <CollectionOverview/>
+    </div>
+    )
 
-        this.state = {
-            collections: SHOP_DATA
-
-        }
-    }
-
-    render() {
-
-        const { collections } = this.state;
-        return (<div className='shop-page'>
-            {
-                collections.map(({ id, ...otherCollectionProps }) => (
-            <CollectionPreview key='id' {...otherCollectionProps} />
-
-            )
-            )
-        }
-        </div>)
-    }
-
-}
-
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollections
+})
 export default ShopPage;
